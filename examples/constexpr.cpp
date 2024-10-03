31cce3326787470af5522eca3f75f6173d9fd7a1
Nikolai <nkutyavin@gmail.com>
2024-10-03 17:50:19 +0200

    Add execution time measurement
    Test how mch time is needed for the algorithm
    at both compile-time and run-time.

--- examples/constexpr.cpp
+++ examples/constexpr.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <chrono>
 #include "prime-numbers.h"
+
+using std::chrono::high_resolution_clock;
 
 int main() {
   // make it works clang++-15 -fconstexpr-steps=999999999 -std=c++20 goto.cpp
+  auto begin= high_resolution_clock::now();
   constexpr std::array<int, 2000> p = primes<2000>();
+  auto end = high_resolution_clock::now();
+
+  std::cout << "Generation time: " << (end - begin)  << std::endl;
   
   for(auto e: p) {
      std::cout << e << " ";
--- examples/runtime.cpp
+++ examples/runtime.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <chrono>
 #include "prime-numbers.h"
 
+using std::chrono::high_resolution_clock;
+
 int main() {
+  auto begin= high_resolution_clock::now();
   std::array<int, 2000> p = primes<2000>();
+  auto end = high_resolution_clock::now();
+
+  std::cout << "Generation time: " << (end - begin)  << std::endl;
   
   for(auto e: p) {
      std::cout << e << " ";
